614106bc4bd7cf65e56501eac70918972a35e262
Backinblacker <coachblacker@gmail.com>
2023-06-23 14:06:45 -0600

    👌 IMPROVE: Removed seconds from the date time stamp

--- src/Components/DisplayPosts/DisplayPosts.jsx
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -4,7 +4,9 @@ import React, { useState } from 'react';
 const DisplayPosts = (props) => {
     const [like, setLike] = useState('inactive')
     const [dislike, setDislike] = useState('inactive')
-    const [currentDateTime, setCurrentDateTime] = useState(new Date().toLocaleString());
+    const [currentDateTime, setCurrentDateTime] = useState(
+        new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', year: 'numeric', month: '2-digit', day: '2-digit' })
+      );
 
     function handleLikeClick() {
         if (like === 'inactive') {
